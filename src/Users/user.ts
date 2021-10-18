cd9ac38708bc82a7218bc058e145743de6b6939b
Pablo <pmoron@consensusgroup.net>
2021-10-18 20:49:22 -0300

    Avance UserSquema

--- src/Users/user.ts
+++ src/Users/user.ts
@@ -69,10 +69,44 @@ const UserSchema = new Schema({
     }
 }, {collection: "Users"})
 
+//  Validacion de longitud de password
 UserSchema.path("password").validate((password: string)=>{
     return password && password.length > 4
 }, "Password should contain at least 4 characters")
 
+//  Aplica una funcion hash a un password
 UserSchema.methods.hashPassword = (password: string): string => {
     return hashSync(password, conf.passwordSalt)
 }
+
+//  Asigna una lista de permisos a un usuario
+UserSchema.methods.grant = function (permissions : string[]): void  {
+    permissions.forEach((permission)=>{
+        if (!this.permissions.includes(permission)) this.permissions.push(permission)
+    })
+}
+
+//  Quita permisos a un usuario
+UserSchema.methods.revoke = function (permissions: string[]): void {
+    permissions.forEach((permission:string, index: number)=>{
+        if(this.permission.include(permission)) this.permissions.splice(index,1) 
+    })
+}
+
+//  Verifica si tiene un permiso
+UserSchema.methods.hasPermission = function (permission: string):boolean{
+    return this.permission.include(permission)
+}
+
+//  Autentica la contraseña de un usuario
+UserSchema.methods.authenticate = function (password: string): boolean{
+    return this.password && this.password === this.hashPassword(password)
+}
+
+//  Actualiza el password de un usuario
+UserSchema.methods.setPassword = function (password: string): void{
+    this.password = this.hashPassword(password)
+    this.updated = Date.now()
+}
+
+export const User = model<IUser>("User", UserSchema)
